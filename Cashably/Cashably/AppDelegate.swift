44e0f5a0143ca843071837d6d4ba5a106272b05b
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-06-19 08:04:37 -0700

    set tabbaritem color

--- Cashably/Cashably/AppDelegate.swift
+++ Cashably/Cashably/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().tintColor = UIColor(red: 0.024, green: 0.792, blue: 0.549, alpha: 1)
         return true
     }
 
