0235a9deb35fa580d48264a4587d6c6062eea8e8
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-06-22 23:00:02 -0700

    keybaor extension

---
+++ Cashably/Cashably/Extensions/UIViewController+Keyboard.swift
@@ -0,0 +1,155 @@
+//
+//  UIViewController+Keyboard.swift
+//  Cashably
+//
+//  Created by apollo on 6/22/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    // MARK: - Notifications
+    
+    ///EZSE: Adds an NotificationCenter with name and Selector
+    open func addNotificationObserver(_ name: String, selector: Selector) {
+        print("addnotificatoinobberser")
+        NotificationCenter.default.addObserver(self, selector: selector, name: NSNotification.Name(rawValue: name), object: nil)
+    }
+    
+    ///EZSE: Removes an NSNotificationCenter for name
+    open func removeNotificationObserver(_ name: String) {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: name), object: nil)
+    }
+    
+    ///EZSE: Removes NotificationCenter'd observer
+    open func removeNotificationObserver() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    #if os(iOS)
+    
+    ///EZSE: Adds a NotificationCenter Observer for keyboardWillShowNotification()
+    ///
+    /// ⚠️ You also need to implement ```keyboardWillShowNotification(_ notification: Notification)```
+    open func addKeyboardWillShowNotification() {
+        self.addNotificationObserver(UIResponder.keyboardWillShowNotification.rawValue, selector: #selector(UIViewController.keyboardWillShowNotification(_:)))
+    }
+    
+    ///EZSE:  Adds a NotificationCenter Observer for keyboardDidShowNotification()
+    ///
+    /// ⚠️ You also need to implement ```keyboardDidShowNotification(_ notification: Notification)```
+    public func addKeyboardDidShowNotification() {
+        self.addNotificationObserver(UIResponder.keyboardDidShowNotification.rawValue, selector: #selector(UIViewController.keyboardDidShowNotification(_:)))
+    }
+    
+    ///EZSE:  Adds a NotificationCenter Observer for keyboardWillHideNotification()
+    ///
+    /// ⚠️ You also need to implement ```keyboardWillHideNotification(_ notification: Notification)```
+    open func addKeyboardWillHideNotification() {
+        self.addNotificationObserver(UIResponder.keyboardWillHideNotification.rawValue, selector: #selector(UIViewController.keyboardWillHideNotification(_:)))
+    }
+    
+    ///EZSE:  Adds a NotificationCenter Observer for keyboardDidHideNotification()
+    ///
+    /// ⚠️ You also need to implement ```keyboardDidHideNotification(_ notification: Notification)```
+    open func addKeyboardDidHideNotification() {
+        self.addNotificationObserver(UIResponder.keyboardDidHideNotification.rawValue, selector: #selector(UIViewController.keyboardDidHideNotification(_:)))
+    }
+    
+    ///EZSE: Removes keyboardWillShowNotification()'s NotificationCenter Observer
+    open func removeKeyboardWillShowNotification() {
+        self.removeNotificationObserver(UIResponder.keyboardWillShowNotification.rawValue)
+    }
+    
+    ///EZSE: Removes keyboardDidShowNotification()'s NotificationCenter Observer
+    open func removeKeyboardDidShowNotification() {
+        self.removeNotificationObserver(UIResponder.keyboardWillShowNotification.rawValue)
+    }
+    
+    ///EZSE: Removes keyboardWillHideNotification()'s NotificationCenter Observer
+    open func removeKeyboardWillHideNotification() {
+        self.removeNotificationObserver(UIResponder.keyboardWillShowNotification.rawValue)
+    }
+    
+    ///EZSE: Removes keyboardDidHideNotification()'s NotificationCenter Observer
+    open func removeKeyboardDidHideNotification() {
+        self.removeNotificationObserver(UIResponder.keyboardWillShowNotification.rawValue)
+    }
+    
+    @objc open func keyboardDidShowNotification(_ notification: Notification) {
+        if let nInfo = (notification as NSNotification).userInfo, let value = nInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            
+            let frame = value.cgRectValue
+            keyboardDidShowWithFrame(frame)
+        }
+    }
+    
+    @objc open func keyboardWillShowNotification(_ notification: Notification) {
+        if let nInfo = (notification as NSNotification).userInfo, let value = nInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            
+            let frame = value.cgRectValue
+            keyboardWillShowWithFrame(frame)
+        }
+    }
+    
+    @objc open func keyboardWillHideNotification(_ notification: Notification) {
+        if let nInfo = (notification as NSNotification).userInfo, let value = nInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            
+            let frame = value.cgRectValue
+            keyboardWillHideWithFrame(frame)
+        }
+    }
+    
+    @objc open func keyboardDidHideNotification(_ notification: Notification) {
+        if let nInfo = (notification as NSNotification).userInfo, let value = nInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            
+            let frame = value.cgRectValue
+            keyboardDidHideWithFrame(frame)
+        }
+    }
+    
+    open func keyboardWillShowWithFrame(_ frame: CGRect) {
+        
+    }
+    
+    open func keyboardDidShowWithFrame(_ frame: CGRect) {
+        
+    }
+    
+    open func keyboardWillHideWithFrame(_ frame: CGRect) {
+        
+    }
+    
+    open func keyboardDidHideWithFrame(_ frame: CGRect) {
+        
+    }
+    
+    //EZSE: Makes the UIViewController register tap events and hides keyboard when clicked somewhere in the ViewController.
+    open func hideKeyboardWhenTappedAround(cancelTouches: Bool = false) {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = cancelTouches
+        view.addGestureRecognizer(tap)
+    }
+    
+    #endif
+    
+    #if os(iOS)
+    
+    @available(*, deprecated: 1.9)
+    public func hideKeyboardWhenTappedAroundAndCancelsTouchesInView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    #endif
+}
+
+extension UIViewController {
+    func hideKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
