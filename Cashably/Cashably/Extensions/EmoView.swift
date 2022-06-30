278f61a3b7f2518347aca8a7faa987d757ecad7b
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-06-30 12:35:41 -0700

    change emo active/inactive

--- Cashably/Cashably/Extensions/EmoView.swift
+++ Cashably/Cashably/Extensions/EmoView.swift
@@ -28,44 +28,23 @@ open class EmoView: UIView {
         for subview in self.subviews {
             if let item = subview as? UIImageView {
                 emo = item
-//                emo.isHidden = true
-//                var view = UILabel()
-//                view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-//                view.backgroundColor = .white
-//                view.layer.compositingFilter = "luminosityBlendMode"
-//                let layer0 = CALayer()
-//                layer0.contents = item.image?.cgImage
-//                layer0.bounds = view.bounds
-//                layer0.position = view.center
-//                view.layer.addSublayer(layer0)
-//
-//                var parent = self
-//
-//                parent.addSubview(view)
-//
-//                view.translatesAutoresizingMaskIntoConstraints = false
-//
-////                view.widthAnchor.constraint(equalToConstant: 20).isActive = true
-////
-////                view.heightAnchor.constraint(equalToConstant: 20).isActive = true
-//
-//                view.centerXAnchor.constraint(equalTo: parent.centerXAnchor, constant: 0).isActive = true
-//
-//                view.centerYAnchor.constraint(equalTo: parent.centerYAnchor, constant: 0).isActive = true
-
-
+                emo.image?.withRenderingMode(.alwaysTemplate)
             }
         }
         
     }
     
-    func active(_ name: String) {
+    func active() {
         backgroundColor = .white
-        emo.image = UIImage(named: "emo_\(name)")
+//        emo.image = UIImage(named: "emo_\(name)")
+//        emo.tintColor = .yellow
+        emo.isHighlighted = true
     }
     
-    func inactive(_ name: String) {
+    func inactive() {
         backgroundColor = .clear
-        emo.image = UIImage(named: "emo_\(name)_inactive")
+//        emo.image = UIImage(named: "emo_\(name)_inactive")
+//        emo.tintColor = .gray
+        emo.isHighlighted = false
     }
 }
