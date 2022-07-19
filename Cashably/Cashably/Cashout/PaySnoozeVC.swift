086e699c28e269ac4ccdad8fae08c701d5331051
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-07-19 10:30:06 -0700

    fix snooze payment crash

--- Cashably/Cashably/Cashout/PaySnoozeVC.swift
+++ Cashably/Cashably/Cashout/PaySnoozeVC.swift
@@ -14,20 +14,13 @@ class PaySnoozeVC: UIViewController, NVActivityIndicatorViewable {
     
     @IBOutlet weak var btnBack: UIButton!
     
-    @IBOutlet weak var chatView: UIView!
-    @IBOutlet weak var anaView: UIView! {
-        didSet {
-            anaView.layer.cornerRadius = 10
-        }
-    }
-    
     @IBOutlet weak var lbAmount: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.drawChat()
+        
         
     }
     
@@ -46,21 +39,7 @@ class PaySnoozeVC: UIViewController, NVActivityIndicatorViewable {
        self.navigationController?.isNavigationBarHidden = false
    }
     
-    func drawChat() {
-        let ringProgressView = RingProgressView(frame: CGRect(x: 0, y: 0, width: 164, height: 164))
-        ringProgressView.startColor = .white
-        ringProgressView.endColor = .white
-        ringProgressView.ringWidth = 25
-        ringProgressView.progress = 0.9
-        ringProgressView.translatesAutoresizingMaskIntoConstraints = false
-        self.chatView.addSubview(ringProgressView)
-        ringProgressView.centerXAnchor.constraint(equalTo: self.chatView.centerXAnchor).isActive = true
-        ringProgressView.centerYAnchor.constraint(equalTo: self.chatView.centerYAnchor).isActive = true
-        ringProgressView.widthAnchor.constraint(equalTo: self.chatView.widthAnchor).isActive = true
-        ringProgressView.heightAnchor.constraint(equalTo: self.chatView.heightAnchor).isActive = true
-        
-    }
-    
+       
     
     @IBAction func actionBack(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
