0f5f6f8eb33f0dd289f688732c896100595f9733
gitdevstar <danielwarhn.online@gmail.com>
2023-02-28 17:00:19 -0800

    change radiobutton

--- Cashably/Cashably/Cashout/CardsVC.swift
+++ Cashably/Cashably/Cashout/CardsVC.swift
@@ -8,36 +8,16 @@
 import Foundation
 import UIKit
 import FittedSheets
-import UIButtonExtension
+import MBRadioButton
 
 class CardsVC: UIViewController {
     
-    @IBOutlet weak var radio1: UIRadioButton! {
-        didSet {
-            radio1.configure()
-            radio1.rightText("Net Banking")
-            
-        }
-    }
-    @IBOutlet weak var radio2: UIRadioButton! {
-        didSet {
-            radio2.configure()
-            radio2.rightText("Credit Card")
-        }
-    }
-    @IBOutlet weak var radio3: UIRadioButton! {
-        didSet {
-            radio3.configure()
-            radio3.rightText("Debit Card")
-        }
-    }
-    @IBOutlet weak var radio4: UIRadioButton! {
-        didSet {
-            radio4.configure()
-            radio4.rightText("UPI")
-        }
-    }
-       
+    @IBOutlet weak var radio1: RadioButton!
+    @IBOutlet weak var radio2: RadioButton!
+    @IBOutlet weak var radio3: RadioButton!
+    @IBOutlet weak var radio4: RadioButton!
+    
+    var groupContainer = RadioButtonContainer()
     
     @IBOutlet weak var cardsCollectionView: UICollectionView! {
         didSet {
@@ -52,13 +32,13 @@ class CardsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        radio1.relate(otherUIRadioButtons: [radio2, radio3, radio4])
-        radio1.button.addTarget(self, action: #selector(selectRadio1(_:)), for: .touchUpInside)
-        radio2.button.addTarget(self, action: #selector(selectRadio2(_:)), for: .touchUpInside)
-        radio3.button.addTarget(self, action: #selector(selectRadio3(_:)), for: .touchUpInside)
-        radio4.button.addTarget(self, action: #selector(selectRadio4(_:)), for: .touchUpInside)
+        radio1.configure()
+        radio2.configure()
+        radio3.configure()
+        radio4.configure()
         
+        groupContainer.addButtons([radio1, radio2, radio3, radio4])
+        groupContainer.selectedButton = radio1
         
     }
     
@@ -72,19 +52,7 @@ class CardsVC: UIViewController {
        super.viewWillDisappear(animated)
        self.navigationController?.isNavigationBarHidden = false
    }
-    
-    @objc func selectRadio1(_ sender: UIRadioButton? = nil) {
-        
-    }
-    @objc func selectRadio2(_ sender: UIRadioButton? = nil) {
-        
-    }
-    @objc func selectRadio3(_ sender: UIRadioButton? = nil) {
-        
-    }
-    @objc func selectRadio4(_ sender: UIRadioButton? = nil) {
-        
-    }
+
     
     @IBAction func actionAdd(_ sender: UIButton) {
         let saveVC = storyboard?.instantiateViewController(withIdentifier: "SaveCardVC") as! SaveCardVC
--- Cashably/Cashably/Extensions/UIRadioButton.swift
+++ Cashably/Cashably/Extensions/UIRadioButton.swift
@@ -6,19 +6,12 @@
 //
 
 import Foundation
-import UIButtonExtension
 import UIKit
+import MBRadioButton
 
-extension UIRadioButton {
-        
+extension RadioButton {
+
     public func configure() {
-        self.color = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)           //changes the color of the outer circle to red.
-        self.selectedColor = UIColor(red: 0.024, green: 0.792, blue: 0.549, alpha: 1) //changes the color of the inner circle to green
-        self.borderWidth = 1        //changes the width of the outer circle to 5
-        self.cornerRadius = 10      //changes the cornerRadius of the button to 20. (no longer a circle)
-        self.selectedSize = 0.5     // changes the ratio of the inner circle size to the outer circle, only available from 0 to 1
-        self.font = UIFont(name: "BRFirma-Medium", size: 15)
-        self.fontSize = 15
-        self.textColor = UIColor(red: 0.151, green: 0.127, blue: 0.2, alpha: 1)
+        radioButtonColor = RadioButtonColor(active: UIColor(red: 0.024, green: 0.792, blue: 0.549, alpha: 1), inactive: UIColor(red: 0.631, green: 0.651, blue: 0.643, alpha: 1))
     }
 }
