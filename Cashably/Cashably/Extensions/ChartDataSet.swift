2b660f35c5d79eec5818b917f550660d45145190
gitdevstar <loganph.work@gmail.com>
2023-05-22 06:59:38 -0700

    update xcode

---
+++ Cashably/Cashably/Extensions/ChartDataSet.swift
@@ -0,0 +1,16 @@
+//
+//  ChartDataSet.swift
+//  Cashably
+//
+//  Created by apollo on 5/21/23.
+//
+
+import Foundation
+import Charts
+//
+//extension ChartDataSet: RangeReplaceableCollection {
+//    public func replaceSubrange<C>(_ subrange: Swift.Range<Int>, with newElements: C) where C : Collection, ChartDataEntry == C.Element {
+//        entries.replaceSubrange(subrange, with: newElements)
+//        notifyDataSetChanged()
+//    }
+//}
